8219d40d8005d10c83db5d05833abfed17e60f0e
Chaman_Raghav <chaman.raghav@vicesoftware.com>
2022-05-06 00:01:40 +0530

    Changed location

--- src/widget/Footer.js
+++ src/widget/Footer.js
@@ -14,8 +14,8 @@ export const Footer = () => {
             </h6>
             <div className="map">
               <iframe
-                title="Satnali_Mahendergarh"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28084.703792453758!2d75.9469537777592!3d28.37130393136319!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3912e904d5acdd83%3A0xb421b893380b69aa!2sSatnali%2C%20Haryana%20123024!5e0!3m2!1sen!2sin!4v1625333546793!5m2!1sen!2sin"
+                title="sector37_gurgaon"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14033.732622445725!2d76.99022378073715!3d28.436356219927756!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d17f1da1a88c5%3A0x39380eec075415fb!2sSector%2037%2C%20Gurugram%2C%20Haryana!5e0!3m2!1sen!2sin!4v1651775421554!5m2!1sen!2sin"
                 width="600"
                 height="450"
                 frameBorder="0"
